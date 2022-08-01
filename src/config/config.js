554a2a6e33f09f3402c1017abe80dd5ccedb7f10
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-08-01 21:45:55 +0200

    added support for action taken when payload doesn't have profiles IDs

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
 const env = require('../utils/env')
 
 module.exports = {
-  EGRESS_URLS: env('EGRESS_URLS', ''),
+  EGRESS_URLS: env('EGRESS_URLS', 'https://testmp.free.beeceptor.com'),
   INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
   INGRESS_PORT: env('INGRESS_PORT', '8080'),
   PROFILE_IDS: env('PROFILE_IDS', '0c86cf8b-a8b2-45b1-ad36-a59c20404b24'),
   ACTION_TYPE: env('ACTION_TYPE', 'forward'),
+  NA_ACTION_TYPE: env('NA_ACTION_TYPE', 'forward'),
   MODULE_NAME: env('MODULE_NAME', 'Melita Profile Filter'),
 }
